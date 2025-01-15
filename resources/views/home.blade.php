7a1e573fc4acb636ced49c8a9e81c48dd55608a6
lucalves0 <114783933+lucalves0@users.noreply.github.com>
2025-01-14 23:11:47 -0300

    Layout Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Fonts Google  --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+    {{-- Ícones --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/home.css')}}">
+
+    <title>Home</title>
+  </head>
+  <body>
+    <main>
+      <div class="menu-bar">
+        <div class="div-1">
+          <a href="#"><i class="fa-solid fa-house"></i>Página Inicial</a>
+          <a href="#"><i class="fa-solid fa-list"></i></i>Histórico</a>
+          <a href="#"><i class="fa-regular fa-clipboard"></i>Listagem</a>
+        </div>
+        <div class="div-2">
+          <a href="#"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>  
+        </div>
+        
+      </div>
+      <div class="container">
+      </div>
+    </main>
+    
+  </body>
+</html>
